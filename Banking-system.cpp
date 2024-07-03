0879702169ed0a1949b10a096bd7d2e90046e8b7
Bereket51 <35872206+bereket-desta@users.noreply.github.com>
2024-07-03 11:29:40 +0300

    Create Banking-system.cpp

---
+++ Banking-system.cpp
@@ -0,0 +1,15 @@
+#include<fstream>
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+// CLASS USED IN PROJECT
+
+class account{
+int acno;
+char name[50];
+double deposit;
+char type;
+
+}
